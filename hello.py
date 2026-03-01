ef1c461280b76d0e7515dcc6b413c73f76180bbb
PRK2012 <pujarajeshkumar2012@gmail.com>
2026-03-01 13:36:01 -0500

    Interactively greet user

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("Hi, this is Puja's first Python program!")
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+print("Hi", name + "!")
+print("You are", age, "years old.")
+print("You are learning Python and GitHub like a pro 😎")
